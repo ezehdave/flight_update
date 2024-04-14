e565906ec9f9e6a8f8bf46424aa64cf4fce58f5e
David <90981355+ezehdave@users.noreply.github.com>
2024-04-14 01:37:29 +0100

    adding files

--- flight/settings.py
+++ flight/settings.py
@@ -84,8 +84,13 @@ WSGI_APPLICATION = 'flight.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'qukxbKhUWMRnAMduvFTeObePBMziUoOR',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '32653',
+
     }
 }
 
